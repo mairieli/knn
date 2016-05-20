02ee57b4a2e4113ed5345aab00e0bc6a1b69db3a
Mairieli Wessel <mairieliw@gmail.com>
2016-05-20 16:09:15 -0300

    adicionada leitura no arquivo

--- src/main/java/br/edu/utfpr/knn/Main.java
+++ src/main/java/br/edu/utfpr/knn/Main.java
@@ -1,13 +1,21 @@
 package br.edu.utfpr.knn;
 
+import br.edu.utfpr.arquivo.ManipuladorArquivo;
+import br.edu.utfpr.bean.Instancia;
+import java.util.List;
+
 /**
  *
  * @author mairieli
  */
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+        List<Instancia> instancias = ManipuladorArquivo.lerArquivo("treino-teste/teste.data");
+        for (Instancia i : instancias) {
+            System.out.println("Classe: " + i.getClasse());
+            System.out.println("Caracteristicas: " + i.getCaracteristicas());
+        }
     }
-    
+
 }
